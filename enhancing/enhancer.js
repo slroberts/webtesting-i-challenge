59289d6ad5ab4831f51a7f6c51387e5e670a1ccb
Shomari Roberts <shomariroberts@gmail.com>
2020-10-07 01:51:06 -0400

    completed MVP & stretch

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,29 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  return {
+    ...item,
+    enhancement: item.enhancement === 20 ? 20 : ++item.enhancement,
+  };
 }
 
 function fail(item) {
-  return { ...item };
+  return {
+    ...item,
+    durability:
+      item.enhancement < 15 ? item.durability - 5 : item.durability - 10,
+    enhancement:
+      item.enhancement > 16 ? item.enhancement - 1 : item.enhancement,
+  };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
-  return { ...item };
+  return {
+    ...item,
+    name: item.enhancement === 0 ? item.name : `[+7] ${item.name}`,
+  };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,138 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe('enhancer functions', () => {
+  describe('repair()', () => {
+    it('should return a new item with the durability restored to 100', () => {
+      const item = enhancer.repair({
+        name: 'item',
+        durability: 50,
+        enhancement: 20,
+      });
+      const actualItem = {
+        name: 'item',
+        durability: 100,
+        enhancement: 20,
+      };
+      expect(item).toStrictEqual(actualItem);
+    });
+  });
+
+  describe('success()', () => {
+    it('item enhancement increases by 1', () => {
+      const item = enhancer.success({
+        name: 'item',
+        durability: 50,
+        enhancement: 19,
+      });
+      const actualItem = {
+        name: 'item',
+        durability: 100,
+        enhancement: 20,
+      };
+      expect(item.enhancement).toBe(actualItem.enhancement);
+    });
+    it('item enhancement level is not changed when 20', () => {
+      const item = enhancer.success({
+        name: 'item',
+        durability: 50,
+        enhancement: 20,
+      });
+      const actualItem = {
+        name: 'item',
+        durability: 100,
+        enhancement: 20,
+      };
+      expect(item.enhancement).toBe(actualItem.enhancement);
+    });
+    it('item durability is not changed', () => {
+      const item = enhancer.success({
+        name: 'item',
+        durability: 100,
+        enhancement: 20,
+      });
+      const actualItem = {
+        name: 'item',
+        durability: 100,
+        enhancement: 20,
+      };
+      expect(item.durability).toBe(actualItem.durability);
+    });
+  });
+
+  describe('fail()', () => {
+    it('should return item durability decreased by 5 if enhancement is less than 15', () => {
+      const item = enhancer.fail({
+        name: 'item',
+        durability: 100,
+        enhancement: 14,
+      });
+      const actualItem = {
+        name: 'item',
+        durability: 95,
+        enhancement: 14,
+      };
+
+      expect(item).toStrictEqual(actualItem);
+    });
+
+    it('should return item durability decreased by 10 if enhancement is 15 or more', () => {
+      const item = enhancer.fail({
+        name: 'item',
+        durability: 100,
+        enhancement: 15,
+      });
+      const actualItem = {
+        name: 'item',
+        durability: 90,
+        enhancement: 15,
+      };
+
+      expect(item).toStrictEqual(actualItem);
+    });
+
+    it('should return item enhancement decreased by 1 if enhancement is more than 16', () => {
+      const item = enhancer.fail({
+        name: 'item',
+        durability: 100,
+        enhancement: 18,
+      });
+      const actualItem = {
+        name: 'item',
+        durability: 90,
+        enhancement: 17,
+      };
+
+      expect(item).toStrictEqual(actualItem);
+    });
+  });
+
+  describe('get()', () => {
+    it('should return a name not modified', () => {
+      const item = enhancer.get({
+        name: 'item',
+        durability: 100,
+        enhancement: 0,
+      });
+      const actualItem = {
+        name: 'item',
+        durability: 100,
+        enhancement: 0,
+      };
+      expect(item).toStrictEqual(actualItem);
+    });
+
+    it('should return a modified name', () => {
+      const item = enhancer.get({
+        name: 'item',
+        durability: 100,
+        enhancement: 1,
+      });
+      const actualItem = {
+        name: '[+7] item',
+        durability: 100,
+        enhancement: 1,
+      };
+      expect(item).toStrictEqual(actualItem);
+    });
+  });
+});
